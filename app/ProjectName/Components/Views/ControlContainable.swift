82eb543ce14d5efff59e66cd8f32116aa6272cec
Distiller <distiller@static.162.252.208.124.cyberlynk.net>
2018-05-03 05:39:40 -0700

    "https://github.com/Raizlabs/ios-template/commit/8ebe86c913ccb4787d75fd03f40f9985b6c950f9"

---
+++ app/ProjectName/Components/Views/ControlContainable.swift
@@ -0,0 +1,54 @@
+//
+//  ControlContainable.swift
+//  ProjectName
+//
+//  Created by Your Name on 7/7/17.
+//  Copyright © 2017 Raizlabs. All rights reserved.
+//
+
+import Foundation
+
+final class ControlContainableScrollView: UIScrollView {
+
+    override func touchesShouldCancel(in view: UIView) -> Bool {
+        if view is UIControl
+            && !(view is UITextInput)
+            && !(view is UISlider)
+            && !(view is UISwitch) {
+            return true
+        }
+
+        return super.touchesShouldCancel(in: view)
+    }
+
+}
+
+final class ControlContainableTableView: UITableView {
+
+    override func touchesShouldCancel(in view: UIView) -> Bool {
+        if view is UIControl
+            && !(view is UITextInput)
+            && !(view is UISlider)
+            && !(view is UISwitch) {
+            return true
+        }
+
+        return super.touchesShouldCancel(in: view)
+    }
+
+}
+
+final class ControlContainableCollectionView: UICollectionView {
+
+    override func touchesShouldCancel(in view: UIView) -> Bool {
+        if view is UIControl
+            && !(view is UITextInput)
+            && !(view is UISlider)
+            && !(view is UISwitch) {
+            return true
+        }
+
+        return super.touchesShouldCancel(in: view)
+    }
+
+}
---
+++ app/ProjectName/Coordinators/OnboardingCoordinator.swift
@@ -0,0 +1,71 @@
+//
+//  OnboardingCoordinator.swift
+//  ProjectName
+//
+//  Created by Your Name on 3/27/17.
+//  Copyright © 2017 Raizlabs. All rights reserved.
+//
+
+import UIKit
+import Services
+
+class OnboardingCoordinator: Coordinator {
+
+    let baseController: UIViewController
+    var childCoordinator: Coordinator?
+    weak var delegate: Delegate?
+
+    init(_ baseController: UIViewController) {
+        self.baseController = baseController
+    }
+
+    func start(animated: Bool, completion: VoidClosure?) {
+        let vc = OnboardingPageViewController(
+            viewModels: OnboardingCoordinator.pageViewModels)
+        vc.delegate = self
+        baseController.present(vc, animated: animated, completion: completion)
+    }
+
+    func cleanup(animated: Bool, completion: VoidClosure?) {
+        baseController.dismiss(animated: animated, completion: completion)
+    }
+
+}
+
+extension OnboardingCoordinator: Actionable {
+
+    enum Action {
+        case didSkipAuth
+        case didRequestJoin
+        case didRequestSignIn
+    }
+
+}
+
+extension OnboardingCoordinator: OnboardingPageViewControllerDelegate {
+
+    func onboardingPageViewController(_ vc: OnboardingPageViewController, didNotify action: OnboardingPageViewController.Action) {
+        switch action {
+        case .skipTapped:
+            notify(.didSkipAuth)
+        case .joinTapped:
+            notify(.didRequestJoin)
+        case .signInTapped:
+            notify(.didRequestSignIn)
+        }
+    }
+
+}
+
+extension OnboardingCoordinator {
+
+    static var pageViewModels: [OnboardingSamplePageViewModel] {
+        let samplePage = OnboardingSamplePageViewModel(
+            header: L10n.Onboarding.Pages.Sample.heading,
+            body: L10n.Onboarding.Pages.Sample.body,
+            asset: Asset.logoKennyLoggins
+        )
+        return [samplePage, samplePage, samplePage]
+    }
+
+}
