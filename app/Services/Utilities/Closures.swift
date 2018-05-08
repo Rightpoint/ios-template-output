dccee07d44683fe708a95a7025135b231efc8757
Distiller <distiller@static.162.252.209.93.cyberlynk.net>
2018-05-08 12:55:01 -0700

    "https://github.com/Raizlabs/ios-template/commit/836dff6ebce15fda6c63b7be71996c20e133578b"

---
+++ app/Services/Utilities/Closures.swift
@@ -0,0 +1,9 @@
+//
+//  Closures.swift
+//  ProjectName
+//
+//  Created by Your Name on 4/5/17.
+//  Copyright © 2017 Raizlabs. All rights reserved.
+//
+
+public typealias VoidClosure = () -> Void
