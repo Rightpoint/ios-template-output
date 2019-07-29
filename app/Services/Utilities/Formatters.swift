63e2636139b390f5a985ab7e10a6fb3d71747a0e
Distiller <distiller@static.192.206.63.132.cyberlynk.net>
2019-07-29 14:22:21 -0700

    "https://github.com/Rightpoint/ios-template/commit/da280b365720f5c1fe36876c847a0e07a642d090"

---
+++ app/Services/Utilities/Formatters.swift
@@ -0,0 +1,15 @@
+//
+//  Formatters.swift
+//  ProjectName
+//
+//  Created by Your Name on 11/1/16.
+//  Copyright © 2017 Raizlabs. All rights reserved.
+//
+
+public enum Formatters {
+
+    public static let ISODateFormatter: ISO8601DateFormatter = {
+        return ISO8601DateFormatter()
+    }()
+
+}
